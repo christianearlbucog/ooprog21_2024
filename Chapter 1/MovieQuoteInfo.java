e0506ea277194e80ad1ba5e003745469eafd5db0
BUCOG_CHRISTIAN EARL <152147468+Hoysts1@users.noreply.github.com>
2024-08-28 08:18:53 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        String quote = "I'm marvel jesus.";
+        String movie = "Deadpool & Wolverine";
+        String character = "Deadpool";
+        int year = 2024;
+
+        System.out.println("Favorite Quote: \"" + quote + "\"");
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    }
+}
